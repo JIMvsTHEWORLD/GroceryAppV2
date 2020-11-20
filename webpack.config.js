5d626a4e17b0c1c6ae16aec208f51002dc6629fb
Jim Armbruster <JMArmbruster@gmail.com>
2020-11-20 13:58:50 -0500

    added additional files and webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './client/index.js',
+    target: 'node',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './build'),
+    },
+    devServer: {
+      publicPath: 'http://localhost:8080/build/', // webpack dev server looks here to find bundle.js
+      proxy: {
+        '/': 'http://localhost:3000',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: path.resolve(__dirname, './node_modules/'),
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.s?css$/i,
+          exclude: path.resolve(__dirname, './node_modules/'),
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+  };
